d95f9f957c79f71e754f95a25aa43418343c8d1d
Casper Audenaert <casper.audenaert@student.hogent.be>
2023-05-07 00:57:19 +0200

    Removed incomming changes

--- weblog_script.py
+++ weblog_script.py
@@ -22,32 +22,19 @@ y = weblogs.iloc[:, -1].values # Labels (Target)
 #Splits de dataset in een training voor de features en de labels
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.25, random_state=1)
 
-<<<<<<< HEAD
-pipeline = Pipeline([
-    ('imputer', SimpleImputer(strategy='mean')),
-    ('scaler', StandardScaler()),
-    ('classifier', DecisionTreeClassifier(random_state=1))
-])
-=======
 pipeline = Pipeline([ #Pipline met verschillende stappen
     ('imputer', SimpleImputer(strategy='median')), #Imputer om de null waarden op te vullen met het gemiddelde
     ('scaler', StandardScaler()), #Veranderd de schaal van de getallen
     ('classifier', DecisionTreeClassifier(random_state=1)) #Het model dat word gebruikt; DecisionTreeClassifier
 ]) 
->>>>>>> e9edba6 (Removed unused imports and added comments)
 
 parameters = { # Mogelijke parameterr voor de pipeline (Alleen voor de classifier in dit script)
     'classifier__max_depth': [3, 4, 5, 6, 7, 8],
     'classifier__criterion': ['gini', 'entropy']
 }
 
-<<<<<<< HEAD
-grid_search = GridSearchCV(pipeline, parameters, cv=5, n_jobs=-1, verbose=1)
-grid_search.fit(X_train, Y_train)
-=======
 grid_search = GridSearchCV(pipeline, parameters, cv=5, n_jobs=-1, verbose=1)# Er word een grid search gedaan met de parameters en de pipeline en deze word dan in een variable geplaatst.
 grid_search.fit(X_train, Y_train) #Fit de gridsearch op de hele dataset
->>>>>>> e9edba6 (Removed unused imports and added comments)
 
 if os.path.exists('model.joblib'):# Controleert dat het model al bestaat, als het al bestaat gaat het verder in de if anders word het model aangemaakt.
     csv_mtime = os.path.getmtime('weblogs.csv')# controleert wanneer het csv bestand is aangepast.
@@ -62,13 +49,7 @@ model = joblib.load('model.joblib')# laad het model in.
 weblogs_test = pd.read_csv('weblogs_test.csv') # Het inlezen van de CSV file als een pandaframe
 robot = 0
 mens = 0
-<<<<<<< HEAD
-#Overloopt alle rijen van de test data en voorspelt of het een robot of een mens is
-for i, row in weblogs_test.iterrows():
-    data = row.values[1:-1]
-=======
 for i, row in weblogs_test.iterrows():#Overloopt elke rij, met de index ervoor
     data = row.values[1:-1]#Neemt de values van elke rij, behalve de eerste waarde 'ID' en de laatse waarde 'ROBOT'
->>>>>>> e9edba6 (Removed unused imports and added comments)
     probability = model.predict_proba([data]) * 100 #Berekent de percentages
     print('Zekerheid Mens:',format(probability[0][0], ".2f"), '%','Zekerheid Robot:',format(probability[0][1], ".2f"), '%')#Print de percentages af
